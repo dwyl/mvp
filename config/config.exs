2240795bfe3ad3f35136ddd189e203ea7de98ff9
nelsonic <nelson+github@dwyl.io>
2019-06-29 23:26:15 +0100

    mix phx.new app https://github.com/nelsonic/time-mvp-phoenix/issues/1

---
+++ config/config.exs
@@ -0,0 +1,30 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :app,
+  ecto_repos: [App.Repo]
+
+# Configures the endpoint
+config :app, AppWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "bue7FwCIX8rDYaT3h4AFGvCK+vPd9Qto427YezF3iHMfaufKylwhweLX/yqkqEww",
+  render_errors: [view: AppWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: App.PubSub, adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
