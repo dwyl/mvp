0fbf07e4bf4e09b9d50f0ebe2a761dc5c950b2fa
SimonLab <simon.labondance@gmail.com>
2019-11-11 16:38:33 +0000

    add .travis.yml configuration file, #231

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule App.MixProject do
     [
       app: :app,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
