b263c29669a18739c55839fa435e1aeba99f0007
nelsonic <nelson+github@dwyl.io>
2019-11-29 21:06:52 +0000

    use :fields v2.0.0 (published hex.pm package) in mix.exs

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule App.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:elixir_auth_google, "~> 0.0.1" },
-      {:fields, "~> 0.1.8"}
+      {:fields, "~> 2.0.0"}
       # {:fields, git: "https://github.com/dwyl/fields.git", branch: "update-ecto"}
     ]
   end
