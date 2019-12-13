94a986cab9294da91268790e3bfb2bbc2a59b402
SimonLab <simon.labondance@gmail.com>
2019-12-13 12:35:26 +0000

    Create a Browser.element app with textarea, #55

--- assets/src/Main.elm
+++ assets/src/Main.elm
@@ -1,5 +1,67 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Browser
+import Html exposing (Html, div, text, textarea)
+import Html.Events exposing (onInput)
 
-main = text "Hello Elm Capture!"
+
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- Model containg the capture text
+
+
+type alias Model =
+    { capture : String }
+
+
+
+-- Msg
+
+
+type Msg
+    = Capture String
+
+
+
+-- init
+
+
+initModel : Model
+initModel =
+    { capture = "" }
+
+
+type alias Flags =
+    ()
+
+
+init : Flags -> ( Model, Cmd Msg )
+init _ =
+    ( initModel, Cmd.none )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Capture text ->
+            ( { model | capture = text }, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ textarea [ onInput Capture ] []
+        ]
