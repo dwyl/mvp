d04c470d1f840f63f8c351f4268772f622e68afb
nelsonic <nelson+github@dwyl.io>
2020-02-05 10:56:24 +0000

    update version of app in mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule App.MixProject do
   def project do
     [
       app: :app,
-      version: "0.1.0",
+      version: "1.0.2",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
