cd6b4b13fd8dfbb13a8b7c352b09ead5e781cbaf
nelsonic <nelson+github@dwyl.com>
2022-06-24 12:15:56 +0100

    exclude phoenix files from test coverage #89

--- lib/app_web/router.ex
+++ lib/app_web/router.ex
@@ -10,18 +10,9 @@ defmodule AppWeb.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
   scope "/", AppWeb do
     pipe_through :browser
 
     get "/", PageController, :index
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", AppWeb do
-  #   pipe_through :api
-  # end
 end
