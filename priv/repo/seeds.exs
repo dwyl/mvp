fe7d4ca8602868449032db0732cfda3b64db823d
nelsonic <nelson+github@dwyl.com>
2022-07-04 22:35:26 +0100

    insert statuses and test person for testing #89

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,20 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias App.{Person, Status}
+
+# Statuses: github.com/dwyl/statuses
+Statuses.parse_json()
+|> Enum.each(fn(s) -> 
+  Status.upsert(%{
+    text: s.text,
+    id: Integer.parse(s.id)
+  })
+end)
+
+
+Person.create(%{
+  givenName: "Beyoncé", 
+  auth_provider: "Google",
+  status_id: 1
+})
