60aeb1a1b65df58c6f1b49d74f643f4a25fb5846
nelsonic <nelson+github@dwyl.com>
2022-07-11 06:16:06 +0100

    bump Elixir version to 1.13.4 (latest) #89

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule App.MixProject do
     [
       app: :app,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
