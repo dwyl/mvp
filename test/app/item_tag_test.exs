45b22b41ed23890333f2235687d6f610e323a41d
SimonLab <simon@dwyl.com>
2022-09-26 12:23:18 +0100

    Add ItemTag schema test
    Add simple test to test field value for ItemTag schema

---
+++ test/app/item_tag_test.exs
@@ -0,0 +1,10 @@
+defmodule App.ItemTagTest do
+  use App.DataCase
+  alias App.ItemTag
+
+  test "valid tag changeset" do
+    item_tag = %ItemTag{item_id: 1, tag_id: 1}
+    assert item_tag.item_id == 1
+    assert item_tag.tag_id == 1
+  end
+end
