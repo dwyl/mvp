8cfd3a993dddb26ca6f4d9f2fc1d6dd1cccde7a7
SimonLab <simon@dwyl.com>
2022-10-05 11:08:20 +0100

    Create check_person_name plug
    Create a router plug which redirect the person to her profile
    to add a name when it is not defined.
    This to make sure that people all have name and can be found
    later on when sharing items

--- lib/app_web/router.ex
+++ lib/app_web/router.ex
@@ -1,5 +1,6 @@
 defmodule AppWeb.Router do
   use AppWeb, :router
+  alias App.Person
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -18,10 +19,16 @@ defmodule AppWeb.Router do
 
   pipeline :authOptional, do: plug(AuthPlugOptional)
   pipeline :verify_loggedin, do: plug(:loggedin)
+  pipeline :check_profile_name, do: plug(:profile_name)
 
   scope "/", AppWeb do
     pipe_through [:browser, :authOptional, :verify_loggedin]
 
+    resources "/profile", ProfileController,
+      except: [:index, :delete],
+      param: "person_id"
+
+    pipe_through [:check_profile_name]
     live "/", AppLive
     resources "/tags", TagController, except: [:show]
     get "/login", AuthController, :login
@@ -32,11 +39,28 @@ defmodule AppWeb.Router do
       param: "person_id"
   end
 
+  # assign to conn the loggedin value used in templates
   defp loggedin(conn, _opts) do
     if not is_nil(conn.assigns[:jwt]) do
       assign(conn, :loggedin, true)
     else
       assign(conn, :loggedin, false)
+    end
+  end
+
+  # Redirect to edit profile to force user to
+  # add name to their profile for sharing items feature
+  defp profile_name(conn, _opts) do
+    person_id = conn.assigns[:person][:id] || 0
+    person = Person.get_person!(person_id)
+
+    if is_nil(person.name) do
+      conn
+      |> put_flash(:info, "Add a name to your profile to allow sharing items")
+      |> redirect(to: "/profile/#{person.person_id}/edit")
+      |> halt()
+    else
+      conn
     end
   end
 end
