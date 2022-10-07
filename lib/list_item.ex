6bf3fbed7fa6153c77d8b0e62d76926cf65a95f5
SimonLab <simon@dwyl.com>
2022-10-07 20:56:19 +0100

    Insert or get person
    Check if person exist before insert

--- lib/list_item.ex
+++ lib/list_item.ex
@@ -1,9 +1,9 @@
-defmodule App.ItemList do
+defmodule App.ListItem do
   use Ecto.Schema
   alias App.{Item, List}
 
   @primary_key false
-  schema "items_lists" do
+  schema "lists_items" do
     belongs_to(:item, Item)
     belongs_to(:list, List)
 
