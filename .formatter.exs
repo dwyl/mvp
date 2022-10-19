b0b6590b92383c81a66cc42846a7b64b14e0b5c7
SimonLab <simon@dwyl.com>
2022-10-19 14:22:00 +0100

    Add LiveView formatter
    - Use formatter to format .heex file
    - Add Github action to check the code is formatted on CI
    - Run mix format

--- .formatter.exs
+++ .formatter.exs
@@ -1,9 +1,10 @@
 [
   import_deps: [:ecto, :phoenix],
+  plugins: [Phoenix.LiveView.HTMLFormatter],
   inputs: [
     "*.{heex,ex,exs}",
     "priv/*/seeds.exs",
-    "{config,lib,test}/**/*.{ex,exs}"
+    "{config,lib,test}/**/*.{heex,ex,exs}"
   ],
   subdirectories: ["priv/*/migrations"],
   line_length: 80
