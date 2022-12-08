96d1147e5c8d560b47dadc830454545013585a5a
SimonLab <simon@dwyl.com>
2022-12-08 15:48:20 +0000

    Test Github env in script
    Check Github environment variables are accessible

--- .github/scripts/review-apps.sh
+++ .github/scripts/review-apps.sh
@@ -1,1 +1,17 @@
 echo "review apps script"
+echo $EVENT_ACTION
+echo $GITHUB_EVENT_NAME
+echo $GITHUB_ACTION
+echo $EVENT_NAME
+
+# if event open or reopen
+#  lauch + deploy
+#
+#  if event is sync
+#    deploy
+#
+# if event is closed
+#   destroy
+#
+# set secrets
+# attach postgres
