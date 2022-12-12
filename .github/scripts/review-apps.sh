9a93e9632c90ced5c9cd02d5057bbacb7c641dd3
SimonLab <simon@dwyl.com>
2022-12-12 11:14:46 +0000

    Update review shell script
    Define flyctl actions in shell script

--- .github/scripts/review-apps.sh
+++ .github/scripts/review-apps.sh
@@ -1,17 +1,27 @@
-echo "review apps script"
-echo $EVENT_ACTION
-echo $GITHUB_EVENT_NAME
-echo $GITHUB_ACTION
-echo $EVENT_NAME
+set -e
 
-# if event open or reopen
-#  lauch + deploy
-#
-#  if event is sync
-#    deploy
-#
-# if event is closed
-#   destroy
-#
-# set secrets
-# attach postgres
+echo "Review App Script"
+# crete "unique" name for fly review app
+app="mvp-pr-$PR_NUMBER"
+secrets="AUTH_API_KEY=$AUTH_API_KEY ENCRYPTION_KEYS=$ENCRYPTION_KEYS"
+
+if [ "$EVENT_ACTION" = "closed" ]; then
+  flyctl apps destroy "$app" -y
+  exit 0
+elif ! flyctl status --app "$app"; then
+  # create application
+  flyctl launch --no-deploy --copy-config --name "$app" --region "$FLY_REGION" --org "$FLY_ORG"
+
+  # attach existing posgres
+  flyctl postgres attach "$FLY_POSTGRES_NAME"
+
+  # add secrets
+  echo $secrets | tr " " "\n" | flyctl secrets import --app "$app"
+
+  # deploy
+  flyctl deploy --app "$app" --region "$FLY_REGION" --image --strategy immediate
+
+else
+  flyctl deploy --app "$app" --region "$FLY_REGION" --image --strategy immediate
+fi
+
