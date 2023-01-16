6f55fda89049b45eaf9526d8796d32e13462f8d3
Luís Arteiro <lucho14garfield@gmail.com>
2023-01-16 20:02:07 +0000

    fix: Mix format. #269

--- lib/app_web/views/error_view.ex
+++ lib/app_web/views/error_view.ex
@@ -7,17 +7,18 @@ defmodule AppWeb.ErrorView do
   #   "Internal Server Error"
   # end
 
-
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.html" becomes
   # "Not Found".
   def template_not_found(template, assigns) do
+    acceptHeader =
+      Enum.at(Plug.Conn.get_req_header(assigns.conn, "content-type"), 0, "")
 
-    acceptHeader = Enum.at(Plug.Conn.get_req_header(assigns.conn, "content-type"), 0, "")
-    isJson = String.contains?(acceptHeader, "application/json") or String.match?(template, ~r/.*\.json/)
+    isJson =
+      String.contains?(acceptHeader, "application/json") or
+        String.match?(template, ~r/.*\.json/)
 
     if isJson do
-
       # If `Content-Type` is `json` but the `Accept` header is not passed, Phoenix considers this as an `.html` request.
       # We want to return a JSON, hence why we check if Phoenix considers this an `.html` request.
       #
@@ -25,15 +26,28 @@ defmodule AppWeb.ErrorView do
       # We try to return a meaningful error if it exists (:reason). It it doesn't, we return the status message from template
       case String.match?(template, ~r/.*\.json/) do
         true ->
-          %{error: Map.get(assigns.conn.assigns.reason, :message, Phoenix.Controller.status_message_from_template(template))}
+          %{
+            error:
+              Map.get(
+                assigns.conn.assigns.reason,
+                :message,
+                Phoenix.Controller.status_message_from_template(template)
+              )
+          }
 
         false ->
           Phoenix.Controller.json(
             assigns.conn,
-            %{error: Map.get(assigns.conn.assigns.reason, :message, Phoenix.Controller.status_message_from_template(template))}
+            %{
+              error:
+                Map.get(
+                  assigns.conn.assigns.reason,
+                  :message,
+                  Phoenix.Controller.status_message_from_template(template)
+                )
+            }
           )
       end
-
     else
       Phoenix.Controller.status_message_from_template(template)
     end
