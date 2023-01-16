94049d95ec00f69d3a5b33183f0c0843fed38804
Luís Arteiro <lucho14garfield@gmail.com>
2023-01-16 20:01:24 +0000

    fix: Fix errors when there is no `reason` property. #269

--- lib/app_web/views/error_view.ex
+++ lib/app_web/views/error_view.ex
@@ -7,10 +7,12 @@ defmodule AppWeb.ErrorView do
   #   "Internal Server Error"
   # end
 
+
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.html" becomes
   # "Not Found".
   def template_not_found(template, assigns) do
+
     acceptHeader = Enum.at(Plug.Conn.get_req_header(assigns.conn, "content-type"), 0, "")
     isJson = String.contains?(acceptHeader, "application/json") or String.match?(template, ~r/.*\.json/)
 
@@ -19,12 +21,18 @@ defmodule AppWeb.ErrorView do
       # If `Content-Type` is `json` but the `Accept` header is not passed, Phoenix considers this as an `.html` request.
       # We want to return a JSON, hence why we check if Phoenix considers this an `.html` request.
       #
-      # If it considers, even though we want to return JSON, we return a JSON with appropriate headers.
+      # If so, we return a JSON with appropriate headers.
+      # We try to return a meaningful error if it exists (:reason). It it doesn't, we return the status message from template
       case String.match?(template, ~r/.*\.json/) do
-        true -> %{error: assigns.conn.assigns.reason.message}
-        false -> Phoenix.Controller.json(assigns.conn, %{error: assigns.conn.assigns.reason.message})
-      end
+        true ->
+          %{error: Map.get(assigns.conn.assigns.reason, :message, Phoenix.Controller.status_message_from_template(template))}
 
+        false ->
+          Phoenix.Controller.json(
+            assigns.conn,
+            %{error: Map.get(assigns.conn.assigns.reason, :message, Phoenix.Controller.status_message_from_template(template))}
+          )
+      end
 
     else
       Phoenix.Controller.status_message_from_template(template)
