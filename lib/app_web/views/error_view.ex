b6f90e90c7cef58cb721b59086388251b4e9f3b1
Luís Arteiro <lucho14garfield@gmail.com>
2023-01-16 18:46:55 +0000

    fix: Returning JSON in ErrorView when appropriate headers are passed. #269

--- lib/app_web/views/error_view.ex
+++ lib/app_web/views/error_view.ex
@@ -10,7 +10,24 @@ defmodule AppWeb.ErrorView do
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.html" becomes
   # "Not Found".
-  def template_not_found(template, _assigns) do
-    Phoenix.Controller.status_message_from_template(template)
+  def template_not_found(template, assigns) do
+    acceptHeader = Enum.at(Plug.Conn.get_req_header(assigns.conn, "content-type"), 0, "")
+    isJson = String.contains?(acceptHeader, "application/json") or String.match?(template, ~r/.*\.json/)
+
+    if isJson do
+
+      # If `Content-Type` is `json` but the `Accept` header is not passed, Phoenix considers this as an `.html` request.
+      # We want to return a JSON, hence why we check if Phoenix considers this an `.html` request.
+      #
+      # If it considers, even though we want to return JSON, we return a JSON with appropriate headers.
+      case String.match?(template, ~r/.*\.json/) do
+        true -> %{error: assigns.conn.assigns.reason.message}
+        false -> Phoenix.Controller.json(assigns.conn, %{error: assigns.conn.assigns.reason.message})
+      end
+
+
+    else
+      Phoenix.Controller.status_message_from_template(template)
+    end
   end
 end
