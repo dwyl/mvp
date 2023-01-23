8eb53ea4a30001eabcd24b394d315c3a98207e24
Luís Arteiro <lucho14garfield@gmail.com>
2023-01-23 18:31:05 +0000

    feat: Adding seed to fix Hopps tests. #256

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,8 +10,10 @@ if not Envar.is_set?("AUTH_API_KEY") do
 end
 
 if Mix.env() == :dev do
+  # Create item
   App.Item.create_item(%{text: "random text", person_id: 0, status: 2})
 
+  # Create timers
   {:ok, _timer} =
     App.Timer.start(%{
       item_id: 1,
@@ -21,4 +23,8 @@ if Mix.env() == :dev do
 
   {:ok, _timer2} =
     App.Timer.start(%{item_id: 1, start: "2023-01-19 15:55:00", stop: nil})
+
+  # Create tags
+  {:ok, _tag} =
+    App.Tag.create_tag(%{text: "tag text", person_id: 0, color: "#FFFFFF"})
 end
