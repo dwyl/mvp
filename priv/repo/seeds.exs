15aee44eda53e49facb802b78e9d0ac122df69fe
Luís Arteiro <lucho14garfield@gmail.com>
2023-01-23 12:28:55 +0000

    fix: Adding seed data and removing SQL file. #278

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,3 +1,16 @@
+# Script for populating the database.
+# You can run it as:
+#
+# mix run priv/repo/seeds.exs
+#
+# Inside the script, you can read and write to any of the repos directly.
+
 if not Envar.is_set?("AUTH_API_KEY") do
   Envar.load(".env")
+end
+
+if Mix.env() == :dev do
+  App.Item.create_item(%{text: "random text", person_id: 0, status: 2})
+  {:ok, _timer} = App.Timer.start(%{item_id: 1, start: "2023-01-19 15:52:00", stop: "2023-01-19 15:52:03"})
+  {:ok, _timer2} = App.Timer.start(%{item_id: 1, start: "2023-01-19 15:55:00", stop: nil})
 end
