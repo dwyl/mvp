fba8d5604e3dc9ffbd1bfac5430fe74aeadbcdc8
Luís Arteiro <lucho14garfield@gmail.com>
2023-01-23 12:32:00 +0000

    fix: Mix format. #278

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,6 +11,14 @@ end
 
 if Mix.env() == :dev do
   App.Item.create_item(%{text: "random text", person_id: 0, status: 2})
-  {:ok, _timer} = App.Timer.start(%{item_id: 1, start: "2023-01-19 15:52:00", stop: "2023-01-19 15:52:03"})
-  {:ok, _timer2} = App.Timer.start(%{item_id: 1, start: "2023-01-19 15:55:00", stop: nil})
+
+  {:ok, _timer} =
+    App.Timer.start(%{
+      item_id: 1,
+      start: "2023-01-19 15:52:00",
+      stop: "2023-01-19 15:52:03"
+    })
+
+  {:ok, _timer2} =
+    App.Timer.start(%{item_id: 1, start: "2023-01-19 15:55:00", stop: nil})
 end
