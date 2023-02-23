8a584c8d50dbb99547631cd51e86e904b7e2cbab
Luís Arteiro <lucho14garfield@gmail.com>
2023-02-23 13:50:18 +0000

    feat: Adding comments and removing seeds. #297

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -8,28 +8,3 @@
 if not Envar.is_set?("AUTH_API_KEY") do
   Envar.load(".env")
 end
-
-if Mix.env() == :dev do
-  # Create item
-  App.Item.create_item_with_tags(%{
-    text: "random text",
-    person_id: 0,
-    status: 2,
-    tags: [%{text: "first_tag", person_id: 0}]
-  })
-
-  # Create timers
-  {:ok, _timer} =
-    App.Timer.start(%{
-      item_id: 1,
-      start: "2023-01-19 15:52:00",
-      stop: "2023-01-19 15:52:03"
-    })
-
-  {:ok, _timer2} =
-    App.Timer.start(%{item_id: 1, start: "2023-01-19 15:55:00", stop: nil})
-
-  # Create tags
-  {:ok, _tag} =
-    App.Tag.create_tag(%{text: "random test", person_id: 0, color: "#FFFFFF"})
-end
