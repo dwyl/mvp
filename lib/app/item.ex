bed4dc0c8b1a6d4f78df7cb9231b86cd0d379ef6
panoramix360 <lucasreis3000@gmail.com>
2023-07-23 19:20:54 -0300

    feat: adds the last item inserted column

--- lib/app/item.ex
+++ lib/app/item.ex
@@ -242,6 +242,7 @@ defmodule App.Item do
     COUNT(distinct i.id) AS "num_items",
     COUNT(distinct t.id) AS "num_timers",
     MIN(i.inserted_at) AS "first_inserted_at",
+    MAX(i.inserted_at) AS "last_inserted_at",
     SUM(EXTRACT(EPOCH FROM (t.stop - t.start))) AS "total_timers_in_seconds"
     FROM items i
     LEFT JOIN timers t ON t.item_id = i.id
