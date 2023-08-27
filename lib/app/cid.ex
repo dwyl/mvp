ac235755b7ee1d8c23fa801f278d009c68fb4c71
nelsonic <nelson+github@dwyl.com>
2023-08-27 07:06:19 +0100

    split put_cid/1 into a separate file to be reused in both items and lists #410

---
+++ lib/app/cid.ex
@@ -0,0 +1,18 @@
+defmodule App.Cid do
+  @moduledoc """
+  Helper functions for adding `cid` to records.
+  """
+
+  @doc """
+  `put_cid/1` as its' name suggests puts the `cid` for the record into the `changeset`.
+  This is done transparently so nobody needs to _think_ about cids.
+  """
+  def put_cid(changeset) do
+    if Map.has_key?(changeset.changes, :cid) do
+      changeset
+    else
+      cid = Cid.cid(changeset.changes)
+      %{changeset | changes: Map.put(changeset.changes, :cid, cid)}
+    end
+  end
+end
