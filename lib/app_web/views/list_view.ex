10a2a6aa9da79837b6eea229e213b937e913dcfe
nelsonic <nelson+github@dwyl.com>
2023-09-15 04:50:12 +0100

    add title_case/1 function for list.name

--- lib/app_web/views/list_view.ex
+++ lib/app_web/views/list_view.ex
@@ -10,6 +10,16 @@ defmodule AppWeb.ListView do
     end
   end
 
+  @doc """
+  `title_case/1` convert "title case" string to "Title Case"
+  ref: https://en.wikipedia.org/wiki/Letter_case#Title_case
+  """
+  def title_case(name) do
+    name
+    |> String.split()
+    |> Enum.map_join(" ", &String.capitalize(&1))
+  end
+
   def format_date(date) do
     App.DateTimeHelper.format_date(date)
   end
