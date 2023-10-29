2d12ffe150f7a0909df0bf69202ab97e2e8d81f4
nelsonic <nelson+github@dwyl.com>
2023-10-29 06:11:44 +0000

    split nav from root.html to separate component for #424 #165

---
+++ lib/app_web/views/nav_view.ex
@@ -0,0 +1,8 @@
+defmodule AppWeb.NavView do
+  use AppWeb, :view
+
+  # Avoid duplicating Tailwind classes and show hot to inline a function call:
+  # defp btn(color) do
+  #   "text-6xl pb-2 w-20 rounded-lg bg-#{color}-500 hover:bg-#{color}-600"
+  # end
+end
