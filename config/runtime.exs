5aebbae213275c99990e771b8ccb66d0b20e2014
nelsonic <nelson+github@dwyl.com>
2026-07-30 12:06:31 +0100

    show_sensitive_data_on_connection_error: true #534

--- config/runtime.exs
+++ config/runtime.exs
@@ -34,7 +34,9 @@ if config_env() == :prod do
     ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
+    socket_options: maybe_ipv6,
+    # Very Temporary To Debug #534
+    show_sensitive_data_on_connection_error: true
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
